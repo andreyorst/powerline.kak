andreyorst/powerline.kak

commit bb4c9bf0f04c59c36fc2a8462be880c44e7f4c2d

reorganize colors

rc/powerline.kak
@@ -34,25 +34,40 @@ used to store powerline configuration before passing it to modeline.
 should never be accessed or modified directly" \
 str powerlinefmt
 
-declare-option -hidden str powerline_color00 black
-declare-option -hidden str powerline_color01 red
-declare-option -hidden str powerline_color02 green
-declare-option -hidden str powerline_color03 yellow
-declare-option -hidden str powerline_color04 blue
-declare-option -hidden str powerline_color05 magenta
-declare-option -hidden str powerline_color06 cyan
-declare-option -hidden str powerline_color07 white
-declare-option -hidden str powerline_color08 bright-black
-declare-option -hidden str powerline_color09 bright-red
-declare-option -hidden str powerline_color10 bright-green
-declare-option -hidden str powerline_color11 bright-yellow
-declare-option -hidden str powerline_color12 bright-blue
-declare-option -hidden str powerline_color13 bright-magenta
-declare-option -hidden str powerline_color14 bright-cyan
-declare-option -hidden str powerline_color15 bright-white
+# Default Module Colors Table
+# ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+# | Name        | foreground | background   |
+# |-------------+------------+--------------|
+# | bufname     | color00    | color03      |
+# | client      | color12    | color13      |
+# | filetype    | color10    | color11      |
+# | git         | color02    | color04      |
+# | line-column | color06    | color09      |
+# | mode-info   | color07    | base_bg (08) |
+# | position    | color01    | color05      |
+# | session     | color14    | color15      |
 
-declare-option -hidden str powerline_next_bg %opt{powerline_color08}
-declare-option -hidden str powerline_base_bg %opt{powerline_color08}
+declare-option -hidden str powerline_color00 black  # bufname fg
+declare-option -hidden str powerline_color01 yellow # position fg
+declare-option -hidden str powerline_color02 green  # git fg
+declare-option -hidden str powerline_color03 yellow # bufname bg
+declare-option -hidden str powerline_color04 black  # git bg
+declare-option -hidden str powerline_color05 black  # position bg
+declare-option -hidden str powerline_color06 cyan   # line-column fg
+declare-option -hidden str powerline_color07 blue   # mode-info fg
+declare-option -hidden str powerline_color08 black  # mode-info bg
+declare-option -hidden str powerline_color09 black  # line-column bg
+declare-option -hidden str powerline_color10 yellow # filetype fg
+declare-option -hidden str powerline_color11 black  # filetype bg
+declare-option -hidden str powerline_color12 blue   # client bg
+declare-option -hidden str powerline_color13 black  # client fg
+declare-option -hidden str powerline_color14 cyan   # session fg
+declare-option -hidden str powerline_color15 black  # session bg
+
+hook global GlobalSetOption powerline_color08=.* %{
+    declare-option -hidden str powerline_next_bg %opt{powerline_color08}
+    declare-option -hidden str powerline_base_bg %opt{powerline_color08}
+}
 
 declare-option -docstring "if 'true' additionally display text formatted position in file, like 'top' and  'bottom'" \
 bool powerline_position_text_format false

rc/themes/base16-gruvbox.kak
@@ -3,24 +3,22 @@
 
 set-option -add global powerline_themes "base16-gruvbox"
 
-define-command -hidden powerline-theme-base16-gruvbox %{
-    set-option global powerline_color00 "rgb:282828"
-    set-option global powerline_color01 "rgb:ebdbb2"
-    set-option global powerline_color02 "rgb:665c54"
-    set-option global powerline_color03 "rgb:a89984"
-    set-option global powerline_color04 "rgb:7c6f64"
-    set-option global powerline_color05 "rgb:504945"
-    set-option global powerline_color06 "rgb:bdae93"
-    set-option global powerline_color07 "rgb:d5c4a1"
-    set-option global powerline_color08 "rgb:3c3836"
-    set-option global powerline_color09 "rgb:282828"
-    set-option global powerline_color10 "rgb:ebdbb2"
-    set-option global powerline_color11 "rgb:665c54"
-    set-option global powerline_color12 "rgb:fbf1c7"
-    set-option global powerline_color13 "rgb:7c6f64"
-    set-option global powerline_color14 "rgb:504945"
-    set-option global powerline_color15 "rgb:bdae93"
-
-    set-option global powerline_base_bg %opt{powerline_color08}
+define-command -override -hidden powerline-theme-base16-gruvbox %{
+    declare-option -hidden str powerline_color00 "rgb:282828" # bufname fg
+    declare-option -hidden str powerline_color01 "rgb:ebdbb2" # position fg
+    declare-option -hidden str powerline_color02 "rgb:bdae93" # git fg
+    declare-option -hidden str powerline_color03 "rgb:a89984" # bufname bg
+    declare-option -hidden str powerline_color04 "rgb:3c3836" # git bg
+    declare-option -hidden str powerline_color05 "rgb:504945" # position bg
+    declare-option -hidden str powerline_color06 "rgb:bdae93" # line-column fg
+    declare-option -hidden str powerline_color07 "rgb:d5c4a1" # mode-info fg
+    declare-option -hidden str powerline_color08 "rgb:3c3836" # mode-info bg
+    declare-option -hidden str powerline_color09 "rgb:504945" # line-column bg
+    declare-option -hidden str powerline_color10 "rgb:ebdbb2" # filetype fg
+    declare-option -hidden str powerline_color11 "rgb:504945" # filetype bg
+    declare-option -hidden str powerline_color12 "rgb:665c54" # client bg
+    declare-option -hidden str powerline_color13 "rgb:ebdbb2" # client fg
+    declare-option -hidden str powerline_color14 "rgb:7c6f64" # session fg
+    declare-option -hidden str powerline_color15 "rgb:fbf1c7" # session bg
 }
 

rc/themes/default.kak
@@ -3,23 +3,21 @@
 set-option -add global powerline_themes "default"
 
 define-command -hidden powerline-theme-default %{
-    declare-option -hidden str powerline_color00 black
-    declare-option -hidden str powerline_color01 red
-    declare-option -hidden str powerline_color02 green
-    declare-option -hidden str powerline_color03 yellow
-    declare-option -hidden str powerline_color04 blue
-    declare-option -hidden str powerline_color05 magenta
-    declare-option -hidden str powerline_color06 cyan
-    declare-option -hidden str powerline_color07 white
-    declare-option -hidden str powerline_color08 bright-black
-    declare-option -hidden str powerline_color09 bright-red
-    declare-option -hidden str powerline_color10 bright-green
-    declare-option -hidden str powerline_color11 bright-yellow
-    declare-option -hidden str powerline_color12 bright-blue
-    declare-option -hidden str powerline_color13 bright-magenta
-    declare-option -hidden str powerline_color14 bright-cyan
-    declare-option -hidden str powerline_color15 bright-white
-
-    declare-option -hidden str powerline_base_bg %opt{powerline_color00}
+    declare-option -hidden str powerline_color00 black  # bufname fg
+    declare-option -hidden str powerline_color01 yellow # position fg
+    declare-option -hidden str powerline_color02 green  # git fg
+    declare-option -hidden str powerline_color03 yellow # bufname bg
+    declare-option -hidden str powerline_color04 black  # git bg
+    declare-option -hidden str powerline_color05 black  # position bg
+    declare-option -hidden str powerline_color06 cyan   # line-column fg
+    declare-option -hidden str powerline_color07 blue   # mode-info fg
+    declare-option -hidden str powerline_color08 black  # mode-info bg
+    declare-option -hidden str powerline_color09 black  # line-column bg
+    declare-option -hidden str powerline_color10 yellow # filetype fg
+    declare-option -hidden str powerline_color11 black  # filetype bg
+    declare-option -hidden str powerline_color12 blue   # client bg
+    declare-option -hidden str powerline_color13 black  # client fg
+    declare-option -hidden str powerline_color14 cyan   # session fg
+    declare-option -hidden str powerline_color15 black  # session bg
 }
 

rc/modules/client.kak
@@ -17,8 +17,8 @@ define-command -override -hidden powerline-client %{ evaluate-commands %sh{
     normal=$kak_opt_powerline_separator
     thin=$kak_opt_powerline_separator_thin
     if [ "$kak_opt_powerline_module_client" = "true" ]; then
-        bg=$kak_opt_powerline_color02
-        fg=$kak_opt_powerline_color12
+        bg=$kak_opt_powerline_color12
+        fg=$kak_opt_powerline_color13
         [ "$next_bg" = "$bg" ] && separator="{$fg,$bg}$thin" || separator="{$bg,${next_bg:-$default}}$normal"
         echo "set-option -add global powerlinefmt %{$separator{$fg,$bg} %val{client} }"
         echo "set-option global powerline_next_bg $bg"

rc/modules/filetype.kak
@@ -17,8 +17,8 @@ define-command -override -hidden powerline-filetype %{ evaluate-commands %sh{
     normal=$kak_opt_powerline_separator
     thin=$kak_opt_powerline_separator_thin
     if [ "$kak_opt_powerline_module_filetype" = "true" ]; then
-        fg=$kak_opt_powerline_color15
-        bg=$kak_opt_powerline_color14
+        fg=$kak_opt_powerline_color10
+        bg=$kak_opt_powerline_color11
         if [ ! -z "$kak_opt_filetype" ]; then
         [ "$next_bg" = "$bg" ] && separator="{$fg,$bg}$thin" || separator="{$bg,${next_bg:-$default}}$normal"
             echo "set-option -add global powerlinefmt %{$separator{$fg,$bg} %opt{filetype} }"

rc/modules/git.kak
@@ -35,8 +35,8 @@ define-command -override -hidden powerline-git %{ evaluate-commands %sh{
     normal=$kak_opt_powerline_separator
     thin=$kak_opt_powerline_separator_thin
     if [ "$kak_opt_powerline_module_git" = "true" ]; then
-        fg=$kak_opt_powerline_color03
-        bg=$kak_opt_powerline_color08
+        fg=$kak_opt_powerline_color02
+        bg=$kak_opt_powerline_color04
         if [ -n "$kak_opt_powerline_branch" ]; then
             [ "$next_bg" = "$bg" ] && separator="{$fg,$bg}$thin" || separator="{$bg,${next_bg:-$default}}$normal"
             echo "set-option -add global powerlinefmt %{$separator{$fg,$bg} %opt{powerline_branch} }"

rc/modules/line_column.kak
@@ -18,7 +18,7 @@ define-command -override -hidden powerline-line-column %{ evaluate-commands %sh{
     thin=$kak_opt_powerline_separator_thin
     if [ "$kak_opt_powerline_module_line_column" = "true" ]; then
         fg=$kak_opt_powerline_color06
-        bg=$kak_opt_powerline_color05
+        bg=$kak_opt_powerline_color09
         [ "$next_bg" = "$bg" ] && separator="{$fg,$bg}$thin" || separator="{$bg,${next_bg:-$default}}$normal"
         echo "set-option -add global powerlinefmt %{$separator{$fg,$bg} %val{cursor_line}{$fg,$bg}:{$fg,$bg}%val{cursor_char_column} }"
         echo "set-option global powerline_next_bg $bg"

commit 9ea368d4e560e7da232a91ab1fdb6d9f909457b6

Fix documentation (and comments) for colour usage: client, session
Colour usage is documented at various places in the repository,
including the README.md file, the main powerline.kak script, and the
colour themes themselves. Looking at those various files, it appears
that the description for a few colours is incorrect: the foreground and
background colours are swapped for the client module in several cases,
and for the session module in all cases.

We could address either by updating the documentation, or by changing
the colours in use to reflect the current documentation. The latter
option would likely introduce colour changes for the users of the
existing themes, so instead we simply update the documentation and do
not change how the colours are used.

Fixes: bb4c9bf0f04c ("reorganize colors")

rc/powerline.kak
@@ -58,13 +58,13 @@ str powerlinefmt
 # | Name        | foreground | background   |
 # |-------------+------------+--------------|
 # | bufname     | color00    | color03      |
-# | client      | color12    | color13      |
+# | client      | color13    | color12      |
 # | filetype    | color10    | color11      |
 # | git         | color02    | color04      |
 # | line-column | color06    | color09      |
 # | mode-info   | color07    | base_bg (08) |
 # | position    | color05    | color01      |
-# | session     | color14    | color15      |
+# | session     | color15    | color14      |
 # | lsp         | color06    | color09      |
 
 declare-option -hidden str powerline_color00 black  # fg: bufname
@@ -81,8 +81,8 @@ declare-option -hidden str powerline_color10 yellow # fg: filetype
 declare-option -hidden str powerline_color11 black  # bg: filetype
 declare-option -hidden str powerline_color12 blue   # bg: client
 declare-option -hidden str powerline_color13 black  # fg: client
-declare-option -hidden str powerline_color14 cyan   # fg: session
-declare-option -hidden str powerline_color15 black  # bg: session
+declare-option -hidden str powerline_color14 cyan   # bg: session
+declare-option -hidden str powerline_color15 black  # fg: session
 declare-option -hidden str powerline_color16 black  # unused
 declare-option -hidden str powerline_color17 yellow # unused
 declare-option -hidden str powerline_color18 green  # unused

rc/themes/base16-gruvbox.kak
@@ -21,8 +21,8 @@ define-command -hidden powerline-theme-base16-gruvbox %{
     declare-option -hidden str powerline_color11 "rgb:504945" # bg: filetype
     declare-option -hidden str powerline_color12 "rgb:665c54" # bg: client
     declare-option -hidden str powerline_color13 "rgb:ebdbb2" # fg: client
-    declare-option -hidden str powerline_color14 "rgb:7c6f64" # fg: session
-    declare-option -hidden str powerline_color15 "rgb:fbf1c7" # bg: session
+    declare-option -hidden str powerline_color14 "rgb:7c6f64" # bg: session
+    declare-option -hidden str powerline_color15 "rgb:fbf1c7" # fg: session
     declare-option -hidden str powerline_color16 "rgb:282828" # unused
     declare-option -hidden str powerline_color17 "rgb:a89984" # unused
     declare-option -hidden str powerline_color18 "rgb:bdae93" # unused

rc/themes/base16.kak
@@ -33,8 +33,8 @@ define-command -hidden powerline-theme-base16 %{ evaluate-commands %sh{
         declare-option -hidden str powerline_color11 ${grey_dark}       # bg: filetype
         declare-option -hidden str powerline_color12 ${purple_dark}     # bg: client
         declare-option -hidden str powerline_color13 ${black_lighter}   # fg: client
-        declare-option -hidden str powerline_color14 ${black_lighter}   # fg: session
-        declare-option -hidden str powerline_color15 ${magenta_dark}    # bg: session
+        declare-option -hidden str powerline_color14 ${black_lighter}   # bg: session
+        declare-option -hidden str powerline_color15 ${magenta_dark}    # fg: session
         declare-option -hidden str powerline_color16 ${black_light}     # unused
         declare-option -hidden str powerline_color17 ${cyan_light}      # unused
         declare-option -hidden str powerline_color18 ${cyan_light}      # unused

rc/themes/default.kak
@@ -20,8 +20,8 @@ define-command -hidden powerline-theme-default %{
     declare-option -hidden str powerline_color11 black  # bg: filetype
     declare-option -hidden str powerline_color12 blue   # bg: client
     declare-option -hidden str powerline_color13 black  # fg: client
-    declare-option -hidden str powerline_color14 cyan   # fg: session
-    declare-option -hidden str powerline_color15 black  # bg: session
+    declare-option -hidden str powerline_color14 cyan   # bg: session
+    declare-option -hidden str powerline_color15 black  # fg: session
     declare-option -hidden str powerline_color16 black  # unused
     declare-option -hidden str powerline_color17 yellow # unused
     declare-option -hidden str powerline_color18 green  # unused

rc/themes/desertex.kak
@@ -20,8 +20,8 @@ define-command -hidden powerline-theme-desertex %{
     declare-option -hidden str powerline_color11 blue       # bg: filetype
     declare-option -hidden str powerline_color12 rgb:fa8072 # bg: client
     declare-option -hidden str powerline_color13 black      # fg: client
-    declare-option -hidden str powerline_color14 black      # fg: session
-    declare-option -hidden str powerline_color15 magenta    # bg: session
+    declare-option -hidden str powerline_color14 black      # bg: session
+    declare-option -hidden str powerline_color15 magenta    # fg: session
     declare-option -hidden str powerline_color16 black      # unused
     declare-option -hidden str powerline_color17 yellow     # unused
     declare-option -hidden str powerline_color18 blue       # unused

rc/themes/github.kak
@@ -20,8 +20,8 @@ define-command -hidden powerline-theme-github %{
     declare-option -hidden str powerline_color11 rgb:A71D5D # bg: filetype
     declare-option -hidden str powerline_color12 rgb:4078C0 # bg: client
     declare-option -hidden str powerline_color13 rgb:F8F8FF # fg: client
-    declare-option -hidden str powerline_color14 rgb:F8F8FF # fg: session
-    declare-option -hidden str powerline_color15 rgb:795DA3 # bg: session
+    declare-option -hidden str powerline_color14 rgb:F8F8FF # bg: session
+    declare-option -hidden str powerline_color15 rgb:795DA3 # fg: session
     declare-option -hidden str powerline_color16 rgb:F8F8FF # unused
     declare-option -hidden str powerline_color17 rgb:4078C0 # unused
     declare-option -hidden str powerline_color18 rgb:4078C0 # unused

rc/themes/greyscale.kak
@@ -44,8 +44,8 @@ define-command -hidden powerline-theme-greyscale %{ evaluate-commands %sh{
         declare-option -hidden str powerline_color11 ${bg1}    # bg: filetype
         declare-option -hidden str powerline_color12 ${bg2}    # bg: client
         declare-option -hidden str powerline_color13 ${fg2}    # fg: client
-        declare-option -hidden str powerline_color14 ${fg}     # fg: session
-        declare-option -hidden str powerline_color15 ${bg3}    # bg: session
+        declare-option -hidden str powerline_color14 ${fg}     # bg: session
+        declare-option -hidden str powerline_color15 ${bg3}    # fg: session
         declare-option -hidden str powerline_color16 ${bg}     # unused
         declare-option -hidden str powerline_color17 ${bg4}    # unused
         declare-option -hidden str powerline_color18 ${fg}     # unused

rc/themes/gruvbox.kak
@@ -42,8 +42,8 @@ define-command -hidden powerline-theme-gruvbox %{ evaluate-commands %sh{
         declare-option -hidden str powerline_color11 ${bg1}    # bg: filetype
         declare-option -hidden str powerline_color12 ${bg2}    # bg: client
         declare-option -hidden str powerline_color13 ${fg2}    # fg: client
-        declare-option -hidden str powerline_color14 ${fg}     # fg: session
-        declare-option -hidden str powerline_color15 ${bg3}    # bg: session
+        declare-option -hidden str powerline_color14 ${fg}     # bg: session
+        declare-option -hidden str powerline_color15 ${bg3}    # fg: session
         declare-option -hidden str powerline_color16 ${bg}     # unused
         declare-option -hidden str powerline_color17 ${bg4}    # unused
         declare-option -hidden str powerline_color18 ${yellow} # unused

rc/themes/kaleidoscope-dark.kak
@@ -107,8 +107,8 @@ define-command -hidden powerline-theme-kaleidoscope-dark %{ evaluate-commands %s
         declare-option -hidden str powerline_color11 ${bg3}    # bg: filetype
         declare-option -hidden str powerline_color12 ${bg2}    # bg: client
         declare-option -hidden str powerline_color13 ${fg2}    # fg: client
-        declare-option -hidden str powerline_color14 ${fg}     # fg: session
-        declare-option -hidden str powerline_color15 ${bg3}    # bg: session
+        declare-option -hidden str powerline_color14 ${fg}     # bg: session
+        declare-option -hidden str powerline_color15 ${bg3}    # fg: session
         declare-option -hidden str powerline_color16 ${bg}     # unused
         declare-option -hidden str powerline_color17 ${bg4}    # unused
         declare-option -hidden str powerline_color18 ${yellow} # unused

rc/themes/lucius.kak
@@ -63,8 +63,8 @@ define-command -hidden powerline-theme-lucius %{ evaluate-commands %sh{
         declare-option -hidden str powerline_color11 ${bg1}    # bg: filetype
         declare-option -hidden str powerline_color12 ${bg2}    # bg: client
         declare-option -hidden str powerline_color13 ${fg2}    # fg: client
-        declare-option -hidden str powerline_color14 ${fg}     # fg: session
-        declare-option -hidden str powerline_color15 ${bg}     # bg: session
+        declare-option -hidden str powerline_color14 ${fg}     # bg: session
+        declare-option -hidden str powerline_color15 ${bg}     # fg: session
         declare-option -hidden str powerline_color16 ${bg}     # unused
         declare-option -hidden str powerline_color17 ${bg4}    # unused
         declare-option -hidden str powerline_color18 ${yellow} # unused

rc/themes/palenight.kak
@@ -62,8 +62,8 @@ define-command -hidden powerline-theme-palenight %{ evaluate-commands %sh{
         declare-option -hidden str powerline_color11 ${bg1}    # bg: filetype
         declare-option -hidden str powerline_color12 ${bg2}    # bg: client
         declare-option -hidden str powerline_color13 ${fg2}    # fg: client
-        declare-option -hidden str powerline_color14 ${fg3}    # fg: session
-        declare-option -hidden str powerline_color15 ${bg3}    # bg: session
+        declare-option -hidden str powerline_color14 ${fg3}    # bg: session
+        declare-option -hidden str powerline_color15 ${bg3}    # fg: session
         declare-option -hidden str powerline_color16 ${bg}     # unused
         declare-option -hidden str powerline_color17 ${bg4}    # unused
         declare-option -hidden str powerline_color18 ${yellow} # unused

rc/themes/red-phoenix.kak
@@ -40,8 +40,8 @@ define-command -hidden powerline-theme-red-phoenix %{ evaluate-commands %sh{
         declare-option -hidden str powerline_color11 ${light_orange1} # bg: filetype
         declare-option -hidden str powerline_color12 ${orange2}       # bg: client
         declare-option -hidden str powerline_color13 ${default}       # fg: client
-        declare-option -hidden str powerline_color14 ${default}       # fg: session
-        declare-option -hidden str powerline_color15 ${orange3}       # bg: session
+        declare-option -hidden str powerline_color14 ${default}       # bg: session
+        declare-option -hidden str powerline_color15 ${orange3}       # fg: session
         declare-option -hidden str powerline_color16 white            # unused
         declare-option -hidden str powerline_color17 ${orange1}       # unused
         declare-option -hidden str powerline_color18 white            # unused

rc/themes/reeder.kak
@@ -35,8 +35,8 @@ define-command -hidden powerline-theme-reeder %{ evaluate-commands %sh{
         declare-option -hidden str powerline_color11 ${grey_dark}     # bg: filetype
         declare-option -hidden str powerline_color12 ${black_light}   # bg: client
         declare-option -hidden str powerline_color13 ${grey_dark}     # fg: client
-        declare-option -hidden str powerline_color14 ${grey_dark}     # fg: session
-        declare-option -hidden str powerline_color15 ${black}         # bg: session
+        declare-option -hidden str powerline_color14 ${grey_dark}     # bg: session
+        declare-option -hidden str powerline_color15 ${black}         # fg: session
         declare-option -hidden str powerline_color16 ${black_light}   # unused
         declare-option -hidden str powerline_color17 ${brown_lighter} # unused
         declare-option -hidden str powerline_color18 ${black_light}   # unused

rc/themes/solarized-dark-termcolors.kak
@@ -20,8 +20,8 @@ define-command -hidden powerline-theme-solarized-dark-termcolors %{
     declare-option -hidden str powerline_color11 bright-cyan   # bg: filetype
     declare-option -hidden str powerline_color12 bright-blue   # bg: client
     declare-option -hidden str powerline_color13 black         # fg: client
-    declare-option -hidden str powerline_color14 black         # fg: session
-    declare-option -hidden str powerline_color15 bright-yellow # bg: session
+    declare-option -hidden str powerline_color14 black         # bg: session
+    declare-option -hidden str powerline_color15 bright-yellow # fg: session
     declare-option -hidden str powerline_color16 black         # unused
     declare-option -hidden str powerline_color17 bright-red    # unused
     declare-option -hidden str powerline_color18 cyan          # unused

rc/themes/solarized-dark.kak
@@ -38,8 +38,8 @@ define-command -hidden powerline-theme-solarized-dark %{ evaluate-commands %sh{
         declare-option -hidden str powerline_color11 ${base1}  # bg: filetype
         declare-option -hidden str powerline_color12 ${base0}  # bg: client
         declare-option -hidden str powerline_color13 ${base02} # fg: client
-        declare-option -hidden str powerline_color14 ${base02} # fg: session
-        declare-option -hidden str powerline_color15 ${base00} # bg: session
+        declare-option -hidden str powerline_color14 ${base02} # bg: session
+        declare-option -hidden str powerline_color15 ${base00} # fg: session
         declare-option -hidden str powerline_color16 ${base02} # unused
         declare-option -hidden str powerline_color17 ${orange} # unused
         declare-option -hidden str powerline_color18 ${cyan}   # unused

rc/themes/solarized-light-termcolors.kak
@@ -20,8 +20,8 @@ define-command -hidden powerline-theme-solarized-light-termcolors %{
     declare-option -hidden str powerline_color11 bright-cyan   # bg: filetype
     declare-option -hidden str powerline_color12 bright-blue   # bg: client
     declare-option -hidden str powerline_color13 black         # fg: client
-    declare-option -hidden str powerline_color14 black         # fg: session
-    declare-option -hidden str powerline_color15 bright-yellow # bg: session
+    declare-option -hidden str powerline_color14 black         # bg: session
+    declare-option -hidden str powerline_color15 bright-yellow # fg: session
     declare-option -hidden str powerline_color16 black         # unused
     declare-option -hidden str powerline_color17 bright-red    # unused
     declare-option -hidden str powerline_color18 cyan          # unused

rc/themes/solarized-light.kak
@@ -38,8 +38,8 @@ define-command -hidden powerline-theme-solarized-light %{ evaluate-commands %sh{
         declare-option -hidden str powerline_color11 ${base1}  # bg: filetype
         declare-option -hidden str powerline_color12 ${base0}  # bg: client
         declare-option -hidden str powerline_color13 ${base02} # fg: client
-        declare-option -hidden str powerline_color14 ${base02} # fg: session
-        declare-option -hidden str powerline_color15 ${base00} # bg: session
+        declare-option -hidden str powerline_color14 ${base02} # bg: session
+        declare-option -hidden str powerline_color15 ${base00} # fg: session
         declare-option -hidden str powerline_color16 ${base02} # unused
         declare-option -hidden str powerline_color17 ${orange} # unused
         declare-option -hidden str powerline_color18 ${cyan}   # unused

rc/themes/tomorrow-night.kak
@@ -39,8 +39,8 @@ define-command -hidden powerline-theme-tomorrow-night %{ evaluate-commands %sh{
         declare-option -hidden str powerline_color11 ${line}   # bg: filetype
         declare-option -hidden str powerline_color12 ${window} # bg: client
         declare-option -hidden str powerline_color13 ${aqua}   # fg: client
-        declare-option -hidden str powerline_color14 ${aqua}   # fg: session
-        declare-option -hidden str powerline_color15 ${line}   # bg: session
+        declare-option -hidden str powerline_color14 ${aqua}   # bg: session
+        declare-option -hidden str powerline_color15 ${line}   # fg: session
         declare-option -hidden str powerline_color16 white     # unused
         declare-option -hidden str powerline_color17 ${window} # unused
         declare-option -hidden str powerline_color18 ${aqua}   # unused

rc/themes/zenburn.kak
@@ -20,8 +20,8 @@ define-command -hidden powerline-theme-zenburn %{
     declare-option -hidden str powerline_color11 rgb:4a4a4a # bg: filetype
     declare-option -hidden str powerline_color12 rgb:2a2a2a # bg: client
     declare-option -hidden str powerline_color13 rgb:cc9393 # fg: client
-    declare-option -hidden str powerline_color14 rgb:7f9f7f # fg: session
-    declare-option -hidden str powerline_color15 rgb:4a4a4a # bg: session
+    declare-option -hidden str powerline_color14 rgb:7f9f7f # bg: session
+    declare-option -hidden str powerline_color15 rgb:4a4a4a # fg: session
     declare-option -hidden str powerline_color16 rgb:cc9393 # unused
     declare-option -hidden str powerline_color17 rgb:2a2a2a # unused
     declare-option -hidden str powerline_color18 rgb:7f9f7f # unused